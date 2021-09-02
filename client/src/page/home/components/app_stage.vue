e559b5fb72fe1f7ba2d8612c43b218582440910b
David <ezioaudiotrebaobao@gmail.com>
2021-09-02 16:57:58 +0800

    前端画布更新3

--- client/src/page/home/components/app_stage.vue
+++ client/src/page/home/components/app_stage.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: David
  * @Date: 2021-08-23 14:55:41
- * @LastEditTime: 2021-08-30 17:02:31
+ * @LastEditTime: 2021-09-02 16:31:52
  * @LastEditors: David
  * @Description: 游戏画布页面
  * @FilePath: /client/src/page/home/components/app_stage.vue
@@ -23,6 +23,15 @@ import useCurrentInstance from '@/hooks/useCurrentInstance'
 import { mapState, mapGetters } from 'vuex'
 import { store } from '@/store/index'
 import KonvaComponent from './draw_table.vue'
+
+interface Pointer {
+  points: number[]
+  stroke: string
+  strokeWidth: number | string
+  lineCap?: string
+  lineJoin?: string
+}
+
 export default defineComponent({
   computed: {
     ...mapState(['lines']),
@@ -40,15 +49,26 @@ export default defineComponent({
     //画布设置
     const stageConfig = ref({ width: 800, height: 700 })
     const isGameHolder = computed(() => store.getters.isGameHolder)
-    console.log(isGameHolder)
     //绘画状态
     const painting = ref(false)
     const stroke = ref('#000')
     const strokeWidth = ref(5)
 
+    //线的集合
+    let lines: Array<Pointer> = ref([])
+
     // 鼠标按下
-    const mousedownHandler = (e: MouseEvent) => {
+    const mousedownHandler = (e: any): void => {
       if (!isGameHolder || !isGameHolder) return
+      let lineObj: Pointer = {
+        points: [e.layerX, e.layerY],
+        stroke: '#000',
+        strokeWidth: 5,
+        lineCap: 'round',
+        lineJoin: 'round',
+      }
+      lines.value.push(lineObj)
+      console.log(lines.value)
     }
 
     return {
